ad09593d46827f1e2445a27613f9dcadae49ee6b
yutianwu <wzxingbupt@gmail.com>
2019-06-18 17:36:30 +0800

    [R4R] Add set version in mutable tree (#8)
    * add set version
    
    * set latest version

--- mutable_tree.go
+++ mutable_tree.go
@@ -224,6 +224,12 @@ func (tree *MutableTree) Load() (int64, error) {
 	return tree.LoadVersion(int64(0))
 }
 
+// SetVersion set current version of the tree. Only used in upgrade
+func (tree *MutableTree) SetVersion(version int64) {
+	tree.version = version
+	tree.ndb.latestVersion = version
+}
+
 // Returns the version number of the latest version found
 func (tree *MutableTree) LoadVersion(targetVersion int64) (int64, error) {
 	roots, err := tree.ndb.getRoots()
